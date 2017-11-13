dfd4f894c3d3f3cc4ae8b703c34b42b183bd18ff
Dalmiro Granas <dalmiro.granias@gmail.com>
2017-11-12 21:34:20 -0300

    adding TestHTTPEndpoint

--- Functions/Trigger.cs
+++ Functions/Trigger.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Serilog;
@@ -15,6 +19,30 @@ namespace AzureFunctions
             log.Info($"Trigger executed at: {DateTime.Now}");
 
             log.Info("Hello from log info!");
+        }
+    }
+
+    public static class TestHTTPEndpoint
+    {
+        [FunctionName("TestHTTPEndPoint")]
+        public static async Task<HttpResponseMessage> Run(HttpRequestMessage req, TraceWriter log)
+        {
+            log.Info($"C# HTTP trigger function processed a request. RequestUri={req.RequestUri}");
+
+            // parse query parameter
+            string name = req.GetQueryNameValuePairs()
+                .FirstOrDefault(q => string.Compare(q.Key, "name", true) == 0)
+                .Value;
+
+            // Get request body
+            dynamic data = await req.Content.ReadAsAsync<object>();
+
+            // Set name to query string or body data
+            name = name ?? data?.name;
+
+            return name == null
+                ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
+                : req.CreateResponse(HttpStatusCode.OK, "Hello " + name);
         }
     }
 
